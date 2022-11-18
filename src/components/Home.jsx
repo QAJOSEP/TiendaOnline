d75298a489fe2095da47cad32bb5f29ecc61c5a1
Jose Developer <papat7442@gmail.com>
2022-11-18 12:35:57 -0600

    2do sprint: skeleton + fetch completo

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Products from './Products';
+import Slider from './Slider';
+
+const Home = () => {
+    return (        
+        <div>
+            <Slider/>
+            <Products/>
+
+        </div>
+    );
+}
+
+export default Home
